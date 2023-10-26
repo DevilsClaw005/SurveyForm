945cd7650e059fac3e04c01a7d96987a2f809de2
Krishnendu Nandi <krishnendunandi2000@gmail.com>
2023-10-26 20:16:24 +0530

    Initial commit

---
+++ SurveyForm/index.js
@@ -0,0 +1,41 @@
+function submitForm() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const dob = document.getElementById("dob").value;
+    const country = document.getElementById("country").value;
+    const genderElements = document.querySelectorAll('input[name="gender"]:checked');
+    const profession = document.getElementById("profession").value;
+    const email = document.getElementById("email").value;
+    const mobile = document.getElementById("mobile").value;
+
+if (firstName && lastName && dob && country && genderElements.length > 0 && profession && email && mobile) {
+    const gender = Array.from(genderElements).map(el => el.value).join(', ');
+
+    
+    const popupContent = `
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Date of Birth:</strong> ${dob}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
+        <p><strong>Profession:</strong> ${profession}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mobile Number:</strong> ${mobile}</p>
+    `;
+    document.getElementById("popup-content").innerHTML = popupContent;
+            document.getElementById("popup").style.display = "block";
+
+            
+            document.getElementById("surveyForm").reset();
+        } else {
+            alert("Please fill out all the required fields.");
+        }
+    }
+
+    function resetForm() {
+        document.getElementById("surveyForm").reset();
+    }
+
+    function closePopup() {
+        document.getElementById("popup").style.display = "none";
+    }
